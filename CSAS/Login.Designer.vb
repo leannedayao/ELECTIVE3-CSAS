274b6b4c9833fd7c07cc93feca867b7981c7d6e4
Leanne Dayao <leannemaedayao@gmail.com>
2020-01-13 00:14:54 +0800

    Initial Commit Version 1.0

---
+++ CSAS/Login.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Minimize = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.LoginLabel = New System.Windows.Forms.Label()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DontHaveAccount = New System.Windows.Forms.Label()
+        Me.SignUpNow = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Minimize
+        '
+        Me.Minimize.BackColor = System.Drawing.Color.Transparent
+        Me.Minimize.BackgroundImage = Global.CSAS.My.Resources.Resources.Minimize
+        Me.Minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Minimize.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Minimize.FlatAppearance.BorderSize = 0
+        Me.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Minimize.ForeColor = System.Drawing.Color.White
+        Me.Minimize.Location = New System.Drawing.Point(577, 6)
+        Me.Minimize.MaximumSize = New System.Drawing.Size(20, 20)
+        Me.Minimize.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.Minimize.Name = "Minimize"
+        Me.Minimize.Size = New System.Drawing.Size(20, 20)
+        Me.Minimize.TabIndex = 2
+        Me.Minimize.UseVisualStyleBackColor = False
+        '
+        'CloseButton
+        '
+        Me.CloseButton.BackColor = System.Drawing.Color.Transparent
+        Me.CloseButton.BackgroundImage = Global.CSAS.My.Resources.Resources.ExitButton
+        Me.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CloseButton.FlatAppearance.BorderSize = 0
+        Me.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CloseButton.Location = New System.Drawing.Point(614, 6)
+        Me.CloseButton.MaximumSize = New System.Drawing.Size(20, 20)
+        Me.CloseButton.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(20, 20)
+        Me.CloseButton.TabIndex = 3
+        Me.CloseButton.UseVisualStyleBackColor = False
+        '
+        'LoginLabel
+        '
+        Me.LoginLabel.AutoSize = True
+        Me.LoginLabel.BackColor = System.Drawing.Color.Transparent
+        Me.LoginLabel.Font = New System.Drawing.Font("Adobe Heiti Std R", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LoginLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(244, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.LoginLabel.Location = New System.Drawing.Point(392, 63)
+        Me.LoginLabel.Name = "LoginLabel"
+        Me.LoginLabel.Size = New System.Drawing.Size(83, 30)
+        Me.LoginLabel.TabIndex = 4
+        Me.LoginLabel.Text = "LOGIN"
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.BackColor = System.Drawing.Color.Transparent
+        Me.UsernameLabel.Font = New System.Drawing.Font("Adobe Heiti Std R", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.UsernameLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.UsernameLabel.Location = New System.Drawing.Point(321, 157)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(96, 23)
+        Me.UsernameLabel.TabIndex = 5
+        Me.UsernameLabel.Text = "Username"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Adobe Heiti Std R", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(325, 185)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(217, 33)
+        Me.TextBox1.TabIndex = 7
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.BackColor = System.Drawing.Color.Transparent
+        Me.PasswordLabel.Font = New System.Drawing.Font("Adobe Heiti Std R", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.PasswordLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PasswordLabel.Location = New System.Drawing.Point(321, 243)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(91, 23)
+        Me.PasswordLabel.TabIndex = 9
+        Me.PasswordLabel.Text = "Password"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Font = New System.Drawing.Font("Adobe Heiti Std R", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(325, 271)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(217, 33)
+        Me.TextBox2.TabIndex = 10
+        '
+        'DontHaveAccount
+        '
+        Me.DontHaveAccount.AutoSize = True
+        Me.DontHaveAccount.BackColor = System.Drawing.Color.Transparent
+        Me.DontHaveAccount.Font = New System.Drawing.Font("Adobe Heiti Std R", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.DontHaveAccount.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.DontHaveAccount.Location = New System.Drawing.Point(280, 388)
+        Me.DontHaveAccount.Name = "DontHaveAccount"
+        Me.DontHaveAccount.Size = New System.Drawing.Size(202, 19)
+        Me.DontHaveAccount.TabIndex = 11
+        Me.DontHaveAccount.Text = "Don't have an account yet?"
+        '
+        'SignUpNow
+        '
+        Me.SignUpNow.AutoSize = True
+        Me.SignUpNow.BackColor = System.Drawing.Color.Transparent
+        Me.SignUpNow.Font = New System.Drawing.Font("Adobe Heiti Std R", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.SignUpNow.ForeColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(78, Byte), Integer), CType(CType(121, Byte), Integer))
+        Me.SignUpNow.Location = New System.Drawing.Point(485, 388)
+        Me.SignUpNow.Name = "SignUpNow"
+        Me.SignUpNow.Size = New System.Drawing.Size(98, 19)
+        Me.SignUpNow.TabIndex = 12
+        Me.SignUpNow.Text = "Sign Up now"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = Global.CSAS.My.Resources.Resources.LoginBlue1
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(362, 338)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 35)
+        Me.Button1.TabIndex = 13
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.CSAS.My.Resources.Resources.LoginSignUp
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(640, 480)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SignUpNow)
+        Me.Controls.Add(Me.DontHaveAccount)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.LoginLabel)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.Minimize)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximumSize = New System.Drawing.Size(640, 480)
+        Me.MinimumSize = New System.Drawing.Size(640, 480)
+        Me.Name = "Login"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Minimize As Button
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents LoginLabel As Label
+    Friend WithEvents UsernameLabel As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PasswordLabel As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DontHaveAccount As Label
+    Friend WithEvents SignUpNow As Label
+    Friend WithEvents Button1 As Button
+End Class
